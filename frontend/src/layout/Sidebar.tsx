0d2cb54c92913056ded48f22a27345a2acf94837
Bob's Programming Academy <97769721+BobsProgrammingAcademy@users.noreply.github.com>
2025-02-27 20:58:54 +0100

    Code refactoring

--- frontend/src/layout/Sidebar.tsx
+++ frontend/src/layout/Sidebar.tsx
@@ -1,4 +1,6 @@
 import { Link } from 'react-router-dom';
+
+// Material Icons
 import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
 import IconButton from '@mui/material/IconButton';
